a450006b31fb1b9c6027aad1a69364bc8d2a4e51
TinaFemea <tina.femea@safekick.com>
2015-12-12 00:11:14 -0600

    day 11 and 12

---
+++ AdventOfCode/Day11/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Day11
+{
+    class Program
+    {
+        static bool IsValid(char[] inputString)
+        {
+            if (inputString.Any(t => t == 'i' ||
+                                     t == 'o' ||
+                                     t == 'l'))
+                return false;
+
+            bool foundStraight = false;
+            for (int i = 0; i < inputString.Length - 2; i++)
+            {
+                if ((inputString[i + 2] == inputString[i + 1] +1) &&
+                    (inputString[i + 1] == inputString[i] + 1))
+                {
+                    foundStraight = true;
+                    break;
+                }
+            }
+            if (!foundStraight)
+                return false;
+
+            int numPairs = 0;
+            char firstPairChar = Char.MinValue;
+            for (int i = 0; i < inputString.Length - 1; i++)
+            {
+                if (inputString[i] != inputString[i + 1]) 
+                    continue;
+                if (inputString[i] == firstPairChar)
+                    continue;
+                firstPairChar = inputString[i];
+                i++;
+                numPairs++;
+                if (numPairs >= 2)
+                    break;
+            }
+
+            return numPairs >= 2;
+        }
+
+        static char[] IncrementMe(char[] theString)
+        {
+            if (theString.Length == 0)
+                return new char[0];
+
+            if (theString[theString.Length - 1] == 'z')
+            {
+                char[] oldArrayBits = theString.Take(theString.Length - 1).ToArray();
+                List<char> newBits = new List<char>(oldArrayBits.Length + 1);
+                newBits.AddRange(IncrementMe(oldArrayBits));
+                newBits.Add('a');
+                return newBits.ToArray();
+            }
+
+            theString[theString.Length-1]++;
+            if (theString[theString.Length - 1] == 'i' ||
+                theString[theString.Length - 1] == 'o' ||
+                theString[theString.Length - 1] == 'l')
+                theString[theString.Length - 1]++;
+
+            return theString;
+        }
+
+        static string FindNext(string inputString)
+        {
+            char[] theString = inputString.ToCharArray();
+
+            while (!IsValid(theString))
+                theString = IncrementMe(theString);
+
+            return new string(theString);
+        }
+        static void Main(string[] args)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+           /* bool isValid = IsValid("hijklmmn".ToCharArray());
+            isValid = IsValid("abbceffg".ToCharArray());
+            isValid = IsValid("abbcegjkbbj".ToCharArray());
+
+            //string nextValid = FindNext("abcdefgh".ToCharArray());
+            //string nextValid = FindNext("ghijklmn".ToCharArray());
+          */
+            string nextValid = FindNext("cqjxjnds");
+
+            timer.Stop();
+            Console.WriteLine("{0} in {1} ms", nextValid, timer.ElapsedMilliseconds);
+            timer.Start();
+
+            nextValid = FindNext(new string(IncrementMe(nextValid.ToCharArray())));
+
+            timer.Stop();
+            Console.WriteLine("{0} in {1} ms", nextValid, timer.ElapsedMilliseconds);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AdventOfCode/Day12/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Day12
+{
+    class Program
+    {
+        static int NextNumber(string input, int index, out long nextNumber)
+        {
+            StringBuilder nextNumberString = new StringBuilder();
+            for (int i = index; i < input.Length; i++)
+            {
+                if (input[i] == '-' || (input[i] >= '0' && input[i] <= '9'))
+                    nextNumberString.Append(input[i]);
+                else
+                {
+                    if (nextNumberString.Length > 0)
+                    {   //  we were numbering, but we're done now.
+                        nextNumber = Int32.Parse(nextNumberString.ToString());
+                        return i + 1;
+                    }
+                }
+            }
+
+            nextNumber = 0;
+            return -1;
+        }
+
+        static int FindPrevUnmatchedOpenBracket(string input, int index)
+        {
+            int numOpens = 0;
+            for (int i = index; i >= 0; i--)
+            {
+                if (input[i] == '}')
+                    numOpens++;
+                if (input[i] == '{')
+                    numOpens--;
+
+                if (numOpens == -1)
+                    return i;
+            }
+            return -1;
+        }
+        static int FindNextUnmatchedCloseBracket(string input, int index)
+        {
+            int numCloses = 0;
+            for (int i = index; i < input.Length; i++)
+            {
+                if (input[i] == '{')
+                    numCloses++;
+                if (input[i] == '}')
+                    numCloses--;
+
+                if (numCloses == -1)
+                    return i;
+            }
+            return -1;
+
+        }
+        static string RemoveRedObjects(string input)
+        {
+            int firstRed = input.IndexOf(":\"red\"");
+            while (firstRed != -1)
+            {
+                int findPreviousCurlyOpenBracket = FindPrevUnmatchedOpenBracket(input, firstRed);
+                int findNextCurlyCloseBracket = FindNextUnmatchedCloseBracket(input, firstRed + 6);
+
+                StringBuilder myBuilder = new StringBuilder(input);
+                int length = findNextCurlyCloseBracket - findPreviousCurlyOpenBracket;
+                myBuilder.Remove(findPreviousCurlyOpenBracket, length + 1);
+                input = myBuilder.ToString();
+                firstRed = input.IndexOf(":\"red\"");
+            }
+
+            return input;
+        }
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText("input.txt");
+            input = RemoveRedObjects(input);
+            long nextNumber;
+            int nextIndex = NextNumber(input, 0, out nextNumber);
+            if (nextIndex != -1)
+            {
+                long total = nextNumber;
+                do
+                {
+                    nextIndex = NextNumber(input, nextIndex, out nextNumber);
+                    total += nextNumber;
+                } while (nextIndex != -1);
+                Console.WriteLine("{0}", total);
+            }
+        }
+    }
+}
