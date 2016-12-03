ee9204c7367572f3dc128d342a7ea4e3e52bf159
Tina Femea <tina.femea@safekick.com>
2016-12-03 12:49:28 -0600

    Day 3!

---
+++ AdventOfCode-2016/Day3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string[] theInput = File.ReadAllLines("input.txt");
+            TriangleChecker checker = new TriangleChecker();
+            int counter = 0;
+
+            foreach (string line in theInput)
+            {
+                string[] numbers = line.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                int a = int.Parse(numbers[0]);
+                int b = int.Parse(numbers[1]);
+                int c = int.Parse(numbers[2]);
+
+                if (checker.IsGoodTriangle(a, b, c))
+                    counter++;
+            }
+
+            Console.Out.WriteLine(counter);
+
+            counter = 0;
+            int[,] matrix = new int[3,3];
+            for (int i = 0; i < theInput.Length; i+=3)
+            {
+                for (int k = 0; k < 3; k++)
+                {
+                    string[] numbers = theInput[i + k].Split(new string[] {" "}, StringSplitOptions.RemoveEmptyEntries);
+                    for (int j = 0; j < 3; j++)
+                        matrix[k, j] = int.Parse(numbers[j]);
+                }
+
+                for (int j = 0; j < 3; j++)
+                {
+                    if (checker.IsGoodTriangle(matrix[0,j], matrix[1,j], matrix[2,j]))
+                        counter++;
+                }
+            }
+
+            Console.Out.WriteLine(counter);
+            Console.In.ReadLine();
+        }
+    }
+}
---
+++ AdventOfCode-2016/Day3/TriangleChecker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day3
+{
+    class TriangleChecker
+    {
+       
+        public bool IsGoodTriangle(int a, int b, int c)
+        {
+            if (a + b <= c)
+                return false;
+            if (b + c <= a)
+                return false;
+            if (c + a <= b)
+                return false;
+
+            return true;
+        }
+    }
+}
